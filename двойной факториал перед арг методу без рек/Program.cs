08c43affaa1aff3baa945b615f9093dc4d0ebe19
elena2020201 <elenarus1992@mail.ru>
2021-05-24 19:53:12 +0300

    Добавьте файлы проекта.

---
+++ двойной факториал перед арг методу без рек/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace двойной_факториал_перед_арг_методу_без_рек
+{
+    class Program
+    {
+        static void show(string txt)
+        {
+            Console.WriteLine(txt);
+        }
+        static int factorial(int n)
+        {
+            int s = 1;
+            for (int k=1; k<=n; k+=2)
+            {
+                s *= k;
+            }
+            return s;
+        }
+        static void Main()
+        {
+            show("Вычисление двойного факториала: ");
+            int m = 5;
+            show(m + "!!=" + factorial(m));
+            Console.ReadLine();
+        }
+    }
+}
